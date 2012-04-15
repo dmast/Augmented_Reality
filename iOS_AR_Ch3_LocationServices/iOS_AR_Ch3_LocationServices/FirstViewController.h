b42c3693ed21b006241f8f582925fc230b312d6d
David Mast <dave.mast@gmail.com>
2012-04-15 10:02:31 -0400

    Test Commit

--- iOS_AR_Ch3_LocationServices/iOS_AR_Ch3_LocationServices/FirstViewController.h
+++ iOS_AR_Ch3_LocationServices/iOS_AR_Ch3_LocationServices/FirstViewController.h
@@ -5,7 +5,7 @@
 //  Created by David M Mast on 4/15/12.
 //  Copyright (c) 2012 Morgan Stanley. All rights reserved.
 //
-
+// Test for source
 #import <UIKit/UIKit.h>
 
 @interface FirstViewController : UIViewController
